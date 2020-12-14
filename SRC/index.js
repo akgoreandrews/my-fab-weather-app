026c7f556c781dda331c2b8404fbfa7aa544fb61
akgoreandrews <73665794+akgoreandrews@users.noreply.github.com>
2020-12-14 20:36:39 +0000

    Revert "Revert "forecast""
    This reverts commit 6d300803f0f1dd63795aa23fb6150510d7aab8f2.

--- SRC/index.js
+++ SRC/index.js
@@ -58,6 +58,8 @@ function searchCity(city) {
   let apiKey = "bf038e802ff968ae367d4b3973b5ce64";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayWeatherCondition);
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
@@ -98,6 +100,33 @@ function displayCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col-2">
+      <h3>
+        ${formatHours(forecast.dt * 1000)}
+      </h3>
+      <img
+        src="http://openweathermap.org/img/wn/${
+          forecast.weather[0].icon
+        }@2x.png"
+      />
+      <div class="weather-forecast-temperature">
+        <strong>
+          ${Math.round(forecast.main.temp_max)}°
+        </strong>
+        ${Math.round(forecast.main.temp_min)}°
+      </div>
+    </div>
+  `;
+  }
+}
 let celsiusTemperature = null;
 
 let form = document.querySelector("form");
